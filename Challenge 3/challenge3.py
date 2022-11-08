e2a47feab5c9f80d1b8d4623efbb759797a16090
Arhan <arhanh1234@gmail.com>
2022-11-08 09:35:46 +0400

    Create challenge3.py

---
+++ Challenge 3/challenge3.py
@@ -0,0 +1,13 @@
+your_name = len(input("What is your name? "))
+their_name = len(input ("What is their name? "))
+
+
+if their_name > your_name: 
+  print("Their name is longer! ")
+
+elif your_name > their_name:
+  print("Your name is longer! ")
+
+elif your_name == their_name:
+  print("Your names are the same length! ")
+
